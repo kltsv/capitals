214b613490b6b060c7648f062067180c7c663db5
Sergey <srg.koltsov@yandex.ru>
2021-05-10 13:22:26 +0300

    Adding assets.

--- lib/data.dart
+++ lib/data.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 import 'models.dart';
@@ -14,4 +15,20 @@ class Api {
         .toList();
     return capitals;
   }
+}
+
+class Assets {
+  static Map<String, List<String>>? _pictures;
+
+  static Future<void> load() async {
+    final raw = await rootBundle.loadString('assets/pictures.json');
+    final assets = jsonDecode(raw) as Map<String, dynamic>;
+    _pictures = <String, List<String>>{
+      for (final asset in assets.entries)
+        asset.key: List<String>.from(asset.value),
+    };
+  }
+
+  static List<String> capitalPictures(String capital) =>
+      _pictures?[capital] ?? <String>[];
 }
