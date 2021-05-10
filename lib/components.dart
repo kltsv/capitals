b16af6665434c67b15eca9cc9bf899406002045c
Sergey <srg.koltsov@yandex.ru>
2021-05-10 15:43:08 +0300

    Game completion.

--- lib/components.dart
+++ lib/components.dart
@@ -219,3 +219,41 @@ class FadeSizedText extends StatelessWidget {
     );
   }
 }
+
+class CompleteWidget extends StatelessWidget {
+  final int score;
+  final int topScore;
+  final VoidCallback? onTap;
+
+  const CompleteWidget({
+    Key? key,
+    required this.score,
+    required this.topScore,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onTap?.call(),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Your result',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          Text(
+            score.toString(),
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          Text(
+            'out of $topScore',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+        ],
+      ),
+    );
+  }
+}
