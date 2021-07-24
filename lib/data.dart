f9dcda9d6fcd5d6d4705adf3f445328c286c7da9
Sergey <srg.koltsov@yandex.ru>
2021-07-24 13:28:42 +0300

    Naive state management via change notifier.

--- lib/data.dart
+++ lib/data.dart
@@ -6,7 +6,9 @@ import 'package:http/http.dart' as http;
 import 'models.dart';
 
 class Api {
-  static Future<List<Country>> fetchCountries() async {
+  const Api();
+
+  Future<List<Country>> fetchCountries() async {
     final rawResponse =
         await http.get(Uri.parse('https://restcountries.eu/rest/v2/all'));
     final response = jsonDecode(rawResponse.body);
