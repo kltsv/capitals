1901a6fc9a983cdd74416349faec27784e5d4612
Sergey <srg.koltsov@yandex.ru>
2021-07-24 16:57:22 +0300

    Palette state.

--- lib/domain/palette.dart
+++ lib/domain/palette.dart
@@ -3,27 +3,45 @@ import 'package:palette_generator/palette_generator.dart';
 
 import 'models.dart';
 
+class PaletteState {
+  final PaletteGenerator? currentPalette;
+  final PaletteGenerator? nextPalette;
+
+  const PaletteState({
+    this.currentPalette,
+    this.nextPalette,
+  });
+
+  PaletteState copyWith({
+    PaletteGenerator? currentPalette,
+    PaletteGenerator? nextPalette,
+  }) =>
+      PaletteState(
+        currentPalette: currentPalette ?? this.currentPalette,
+        nextPalette: nextPalette ?? this.nextPalette,
+      );
+}
+
 class PaletteLogic extends ChangeNotifier {
   static const _defaultColor = Colors.grey;
 
-  PaletteGenerator? _currentPalette;
-  PaletteGenerator? _nextPalette;
+  var state = PaletteState();
 
-  ColorPair get colors => _currentPalette != null
-      ? _buildColors(_currentPalette)
+  ColorPair get colors => state.currentPalette != null
+      ? _buildColors(state.currentPalette)
       : ColorPair(_defaultColor, _defaultColor);
 
   Future<void> updatePalette(ImageProvider current, ImageProvider? next) async {
-    final crt = _currentPalette == null
+    final crt = state.currentPalette == null
         ? await PaletteGenerator.fromImageProvider(current)
-        : _nextPalette;
+        : state.nextPalette;
     final _next =
         next != null ? await PaletteGenerator.fromImageProvider(next) : null;
-    _setState(() {
-      _currentPalette = crt;
-      _nextPalette = _next;
-    });
+    _setState(() => _updatePalettes(crt, _next));
   }
+
+  void _updatePalettes(PaletteGenerator? current, PaletteGenerator? next) =>
+      state = state.copyWith(currentPalette: current, nextPalette: next);
 
   ColorPair _buildColors(PaletteGenerator? palette) {
     var mainColor = palette?.mutedColor?.color;
