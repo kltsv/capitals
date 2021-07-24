da6f950de7226e66c0f3f2c901c7e3e6eabf7ad6
Sergey <srg.koltsov@yandex.ru>
2021-07-24 15:16:50 +0300

    No duplicating colors field.

--- lib/domain/palette.dart
+++ lib/domain/palette.dart
@@ -4,28 +4,31 @@ import 'package:palette_generator/palette_generator.dart';
 import 'models.dart';
 
 class PaletteLogic extends ChangeNotifier {
-  PaletteGenerator? currentPalette;
-  PaletteGenerator? nextPalette;
+  static const _defaultColor = Colors.grey;
 
-  ColorPair colors = ColorPair(Colors.grey, Colors.grey);
+  PaletteGenerator? _currentPalette;
+  PaletteGenerator? _nextPalette;
+
+  ColorPair get colors => _currentPalette != null
+      ? _buildColors(_currentPalette)
+      : ColorPair(_defaultColor, _defaultColor);
 
   Future<void> updatePalette(ImageProvider current, ImageProvider? next) async {
-    final crt = currentPalette == null
+    final crt = _currentPalette == null
         ? await PaletteGenerator.fromImageProvider(current)
-        : nextPalette;
+        : _nextPalette;
     final _next =
         next != null ? await PaletteGenerator.fromImageProvider(next) : null;
     _setState(() {
-      currentPalette = crt;
-      nextPalette = _next;
-      colors = _buildColors(crt);
+      _currentPalette = crt;
+      _nextPalette = _next;
     });
   }
 
   ColorPair _buildColors(PaletteGenerator? palette) {
     var mainColor = palette?.mutedColor?.color;
     var secondColor = palette?.vibrantColor?.color;
-    final defaultColor = mainColor ?? secondColor ?? Colors.grey;
+    final defaultColor = mainColor ?? secondColor ?? _defaultColor;
     mainColor = mainColor ?? defaultColor;
     secondColor = secondColor ?? defaultColor;
     return ColorPair(mainColor, secondColor);
