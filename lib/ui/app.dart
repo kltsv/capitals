4bd654947737aead50d610989ce6a516d17d9b4a
Sergey <srg.koltsov@yandex.ru>
2021-07-25 13:47:08 +0300

    StreamProvider + Consumer + Selector.

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,5 +1,7 @@
+import 'package:capitals/domain/assemble.dart';
 import 'package:capitals/domain/game.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'components.dart';
 import 'home_page.dart';
@@ -15,15 +17,31 @@ class _AppState extends State<App> {
   var _dark = false;
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: _appName,
-        builder: (context, child) => ThemeSwitch(
-          isDark: _dark,
-          child: child,
-          onToggle: () => setState(() => _dark = !_dark),
+  Widget build(BuildContext context) => MultiProvider(
+        providers: [
+          StreamProvider.value(
+            value: assemble.itemsLogic.stream,
+            initialData: assemble.itemsLogic.state,
+          ),
+          StreamProvider.value(
+            value: assemble.palette.stream,
+            initialData: assemble.palette.colors,
+          ),
+          StreamProvider.value(
+            value: assemble.game.stream,
+            initialData: assemble.game.state,
+          )
+        ],
+        child: MaterialApp(
+          title: _appName,
+          builder: (context, child) => ThemeSwitch(
+            isDark: _dark,
+            child: child,
+            onToggle: () => setState(() => _dark = !_dark),
+          ),
+          theme:
+              ThemeData(brightness: _dark ? Brightness.dark : Brightness.light),
+          home: HomePage(),
         ),
-        theme:
-            ThemeData(brightness: _dark ? Brightness.dark : Brightness.light),
-        home: HomePage(),
       );
 }
