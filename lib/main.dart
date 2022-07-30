797f01f01c828a3067d796a9112bd9f0efa5ce6a
Sergey <srg.koltsov@yandex.ru>
2022-07-30 20:30:57 +0300

    Add logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:capitals/domain/assemble.dart';
+import 'package:capitals/logger.dart';
 import 'package:flutter/material.dart';
 
 import 'ui/app.dart';
 
 void main() {
+  initLogger();
   setup();
   runApp(const App());
 }
